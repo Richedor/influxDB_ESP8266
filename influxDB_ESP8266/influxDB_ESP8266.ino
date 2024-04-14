497ca5b7fc7ac4b178ffecf9485fc697fdafe23c
Richedor <77790992+Richedor@users.noreply.github.com>
2024-04-14 23:52:38 +0100

    Update influxDB_ESP8266.ino

--- influxDB_ESP8266/influxDB_ESP8266.ino
+++ influxDB_ESP8266/influxDB_ESP8266.ino
@@ -8,9 +8,6 @@
  * Licence : MIT
  */
 
-#if defined(ESP32)
-  // Inclusion de la bibliothèque WiFiMulti pour la gestion de plusieurs points d'accès Wi-Fi (pour ESP32)
-  #include <WiFiMulti.h>
 #elif defined(ESP8266)
   // Inclusion de la bibliothèque ESP8266WiFiMulti pour la gestion de plusieurs points d'accès Wi-Fi (pour ESP8266)
   #include <ESP8266WiFiMulti.h>
